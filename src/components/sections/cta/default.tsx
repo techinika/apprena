e81c3645110d1c7cb641384782916f7d7853af65
songa1 <songachillethe1her@gmail.com>
2025-03-14 14:32:19 +0200

    I have made a few changes on the homepage

--- src/components/sections/cta/default.tsx
+++ src/components/sections/cta/default.tsx
@@ -3,8 +3,59 @@ import { Button } from "@/components/ui/button";
 import Glow from "@/components/ui/glow";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { z } from "zod";
+import { AuthError, createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/db/firebase";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import React from "react";
+
+const loginSchema = z.object({
+  email: z.string().email("Invalid email format"),
+});
+
+type LoginFormValues = z.infer<typeof loginSchema>;
 
 export default function CTA() {
+  const [success, setSuccess] = React.useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormValues>({
+    resolver: zodResolver(loginSchema),
+  });
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setSuccess(false);
+    }, 10000);
+  }, [success]);
+
+  const handleSubscribe = async (data: LoginFormValues) => {
+    try {
+      await createUserWithEmailAndPassword(auth, data.email, "password");
+      setSuccess(true);
+    } catch (error: unknown) {
+      if (typeof error === "object" && error !== null && "code" in error) {
+        const firebaseError = error as AuthError;
+        if (firebaseError.message === "auth/email-already-in-use") {
+          toast("Email already in use.");
+        } else if (firebaseError.message === "auth/weak-password") {
+          toast("Password is too weak.");
+        } else {
+          toast("An error occurred. Please try again later.");
+          console.error("Registration error:", error);
+        }
+      } else {
+        toast("An unexpected error occurred.");
+      }
+    }
+  };
+
   return (
     <Section className="relative w-full overflow-hidden">
       <div className="relative z-10 mx-auto flex max-w-container flex-col items-center gap-6 text-center sm:gap-10">
@@ -15,16 +66,37 @@ export default function CTA() {
           You will get to hear from us every day, with latest features and news.
         </p>
         <div className="flex flex-col items-center gap-4 self-stretch">
-          <div className="flex w-full max-w-[420px] gap-2 items-center">
-            <Input
-              type="email"
-              placeholder="Email address"
-              className="grow border-brand/20 bg-foreground/10 z-50"
-            />
-            <Button variant="default" size="default" asChild>
-              <Link href="/">Subscribe</Link>
-            </Button>
-          </div>
+          {!success && (
+            <div className="flex w-full max-w-[420px] gap-2 items-center">
+              <Input
+                type="email"
+                placeholder="Email address"
+                className="grow border-brand/20 bg-foreground/10 z-50"
+                {...register("email")}
+              />
+
+              <Button
+                variant="default"
+                size="default"
+                className="z-50"
+                onClick={handleSubmit(handleSubscribe)}
+                asChild
+              >
+                <Link href="/">Subscribe</Link>
+              </Button>
+            </div>
+          )}
+          {success && (
+            <div>
+              <p className="text-green-500">
+                Thank you for subscribing! We will reach out to you on your
+                email!
+              </p>
+            </div>
+          )}
+          {errors.email && (
+            <p className="text-red-500 text-sm">{errors.email.message}</p>
+          )}
           <p className="text-xs text-muted-foreground">
             We will never spam you, only send you relevant content.
           </p>
