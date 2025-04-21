caf14e1f98b16f3b78fd70451f691b024553e6ad
songa1 <songachillethe1her@gmail.com>
2025-04-21 23:59:03 +0200

    Added login information while logging in

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { UAParser } from "ua-parser-js";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -11,4 +12,20 @@ export const generateSlug = (str: string) => {
     .replace(/[^\w\s]/g, "")
     .replace(/\s+/g, "-")
     .trim();
+};
+
+export const getDeviceInfo = async () => {
+  const parser = new UAParser();
+  const result = parser.getResult();
+
+  const device = result.device?.model || "Unknown";
+  const browser = result.browser?.name || "Unknown";
+  const os = result.os?.name || "Unknown";
+
+  return {
+    location: "", // Can't get without external API
+    device,
+    browser,
+    os,
+  };
 };
