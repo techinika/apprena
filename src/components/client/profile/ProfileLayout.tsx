5d077a7a25600fb0f565a364108f101166ca68ad
songa1 <songachillethe1her@gmail.com>
2025-04-21 03:06:28 +0200

    I have made some changes

--- src/components/client/profile/ProfileLayout.tsx
+++ src/components/client/profile/ProfileLayout.tsx
@@ -7,7 +7,6 @@ import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { useAuth } from "@/lib/AuthContext";
 import { Metadata } from "next";
-import Image from "next/image";
 import Link from "next/link";
 
 export const metadata: Metadata = {
